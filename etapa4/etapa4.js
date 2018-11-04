559e424963c68d18ce5a2f5cdf76c40f7b02d984
gabriel1499morais@gmail.com <gabrielmorais1499@gmail.com>
2018-11-03 21:09:48 -0300

    etapa 4

---
+++ etapa4/etapa4.js
@@ -0,0 +1,73 @@
+//windowWidth: 1326
+//windowHeight: 639
+
+var xJogador = 60;
+var yJogador = 320;
+var xObstaculo = 1400;
+var yObstaculo = 290;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);    
+  background(0);                              
+  //textSize(16);
+    
+  //Jogador
+  ellipse(xJogador, yJogador, 50, 50);        
+  fill(255, 255, 255);                        
+  
+  //Obstáculo
+  rect (xObstaculo, yObstaculo, 50, 50);                   
+  fill(255, 255, 255);  
+  
+  //Informações na tela:
+  textSize(20);
+  textStyle(BOLD);
+  text('PONTUAÇÃO: ', 50, 50);
+  text('VIDAS: ', 1150, 50);    
+}
+  
+function draw() {
+  
+   //Fazer obstáculo se mover
+   xObstaculo = xObstaculo - 10;
+   if (xObstaculo<0){
+     xObstaculo = 1400;
+   }
+   
+   //Fazer jogador mover para frente
+   if (keyIsDown(RIGHT_ARROW)){
+     xJogador += 10;
+   }
+   
+   //Fazer jogador mover para trás
+   if (keyIsDown(LEFT_ARROW)){
+    xJogador -= 10;
+   }
+   
+   //Fazer jogador mover para cima 
+   if (keyIsDown(UP_ARROW)){
+     yJogador -= 10;
+   }
+   
+   //Fazer jogador mover para baixo
+   if (keyIsDown(DOWN_ARROW)){
+     yJogador += 10;
+   }
+   
+   clear();           //Limpa a tela após o movimento
+   setup();
+   
+   /*
+    *Verificar tamanho da tela
+    *text (windowHeight, 50, 50);
+    *text(windowWidth, 100, 50);
+   */
+   
+   
+    /*
+     *Verificar coordenadas do mouse
+     *text(mouseX, 50, 50);
+     *text(mouseY, 100, 50);
+    */
+   
+}
