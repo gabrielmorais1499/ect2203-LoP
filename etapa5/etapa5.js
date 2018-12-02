541c504286b19379b70def9dd93c371d2ba4c92a
gabriel <gabrielmorais1499@gmail.com>
2018-12-02 13:33:12 -0300

    etapa5 OK

--- etapa5/etapa5.js
+++ etapa5/etapa5.js
@@ -5,6 +5,10 @@ var xJogador = 60;
 var yJogador = 320;
 var xObstaculo = 1400;
 var yObstaculo = 290;
+var xTiro=1400;
+var yTiro=640;
+
+var tiro=false;
 
 var vidas=5;
 var pontos=0;
@@ -14,17 +18,8 @@ var nivel=1;
 function setup() {
   createCanvas(windowWidth, windowHeight);    
   background(0);                              
-  //textSize(16);
-    
-  //Jogador
-  ellipse(xJogador, yJogador, 50, 50);        
-  fill(255, 255, 255);                        
+  //textSize(16); 
   
-  //Obstáculo
-  rect (xObstaculo, yObstaculo, 50, 50);                   
-  fill(255, 255, 255);  
-  
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //Informações na tela:
   textSize(20);
   textStyle(BOLD);
@@ -32,51 +27,74 @@ function setup() {
   text('VIDAS: '+vidas, 1150, 50);
   textSize(13);  
   text('Nível '+nivel, 1230, 589);
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
   
 function draw() {
+  display(); 
+  move();
+  clear();           //Limpa a tela após o movimento
+  setup();
+  display();
+   
+  /*
+  *Verificar tamanho da tela
+  *text (windowHeight, 50, 50);
+  *text(windowWidth, 100, 50);
+  */
+}
+
+function display(){
+  //Jogador
+  fill(255, 255, 255);
+  ellipse(xJogador, yJogador, 50, 50);                                 
+  //Obstáculo
+  rect (xObstaculo, yObstaculo, 50, 50);
+
+  if(tiro==true){
+    fill(255,0, 0);
+    noStroke();
+    ellipse(xTiro, yTiro, 20, 20);
+  }
+}
+
+function move(){
+  //Fazer obstáculo se mover
+  xObstaculo = xObstaculo - 5;
+  if (xObstaculo<0){
+    xObstaculo = 1400;
+  }
   
-   //Fazer obstáculo se mover
-   xObstaculo = xObstaculo - 10;
-   if (xObstaculo<0){
-     xObstaculo = 1400;
-   }
-   
-   //Fazer jogador mover para frente
-   if (keyIsDown(RIGHT_ARROW)){
-     xJogador += 10;
-   }
-   
-   //Fazer jogador mover para trás
-   if (keyIsDown(LEFT_ARROW)){
-    xJogador -= 10;
-   }
-   
-   //Fazer jogador mover para cima 
-   if (keyIsDown(UP_ARROW)){
-     yJogador -= 10;
-   }
-   
-   //Fazer jogador mover para baixo
-   if (keyIsDown(DOWN_ARROW)){
-     yJogador += 10;
-   }
-   
-   clear();           //Limpa a tela após o movimento
-   setup();
-   
-   /*
-    *Verificar tamanho da tela
-    *text (windowHeight, 50, 50);
-    *text(windowWidth, 100, 50);
-   */
-   
-   
-    /*
-     *Verificar coordenadas do mouse
-     *text(mouseX, 50, 50);
-     *text(mouseY, 100, 50);
-    */
-   
+  //Fazer jogador mover para frente
+  if (keyIsDown(RIGHT_ARROW)){
+    xJogador += 8;
+  }
+  
+  //Fazer jogador mover para trás
+  if (keyIsDown(LEFT_ARROW)){
+   xJogador -= 8;
+  }
+  
+  //Fazer jogador mover para cima 
+  if (keyIsDown(UP_ARROW)){
+    yJogador -= 8;
+  }
+  
+  //Fazer jogador mover para baixo
+  if (keyIsDown(DOWN_ARROW)){
+    yJogador += 8;     
+  }
+
+  if(keyIsDown(32) && xTiro>=width){
+    tiro=true;
+    xTiro=xJogador;
+    yTiro=yJogador;    
+  }
+
+  if(tiro==true){
+    xTiro+=10;
+  }
+
+  if(xTiro>=width){
+    tiro=false;
+  }
 }
